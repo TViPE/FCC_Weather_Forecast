b145aa464b739da6c5a5d96536d5d40d27df3e26
TRI VI <trivi2710@gmail.com>
2017-05-09 21:30:39 -0400

    Remove old code from app_01 js
    Chore: Remove old code from app_01.js

--- js/app_01.js
+++ js/app_01.js
@@ -1,19 +1,4 @@
 var app = angular.module("weatherForecast", []);
-
-// app.factory("WeatherApi", function($http){
-// 	var obj = {};
-// 	obj.getCurrent = function(city_zipcode){
-// 		var api = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=";
-// 		var units = "&units=metric";
-// 		var appid = "&appid=fedb5f074f6b6256d41bb8cd72d8cc81";
-// 		var cb = "&callback=JSON_CALLBACK";
-
-// 		return $http.jsonp(api + city_zipcode + units+ appid + cb);
-// 	};
-
-// 	return obj;
-// });
-
 app.controller("weatherCtrl", function($scope, $http){
 	
 	
@@ -44,26 +29,4 @@ app.controller("weatherCtrl", function($scope, $http){
 			console.log("Error " + status);
 		})
 	}
-	// $scope.Data = {};
-	// WeatherApi.getCurrent($scope.city_zipcode).success(function(data){
-	// 	currentWeather(data);
-	// });
-
-	// function currentWeather(data){
-	// 	$scope.dataArray = data.list;
-	// 	$scope.dataArray.forEach(function(obj){
-	// 		$scope.Data.temp = obj.temp.day;
-	// 		$scope.Data.minTemp = obj.temp.min;
-	// 		$scope.Data.maxTemp = obj.temp.max;
-	// 		$scope.Data.pressure = obj.pressure;
-	// 		$scope.Data.humidity = obj.humidity;
-	// 		$scope.Data.des = obj.weather[0].description;
-	// 		$scope.Data.windSpeed = obj.speed;
-
-	// 		// May add conversion from C deg to F deg later
-	// 	})
-		
-
-
-	// }
 });
